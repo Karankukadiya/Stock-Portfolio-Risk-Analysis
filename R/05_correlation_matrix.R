dee45e145b2ed2660948006c975143994993fd1b
Karan Kukadiya <153797055+Karankukadiya@users.noreply.github.com>
2026-07-26 22:20:45 +0530

    Delete R directory

--- R/05_correlation_matrix.R
+++
@@ -1,9 +0,0 @@
-library(reshape2)
-
-returns_wide <- returns_daily %>%
-    pivot_wider(names_from = symbol, values_from = daily_return)
-
-corr_matrix <- cor(returns_wide[,-1], use = "complete.obs")
-
-corr_long <- melt(corr_matrix)
-write.csv(corr_long, "data/processed/correlation_matrix.csv", row.names = FALSE)
--- R/07_export_for_tableau.R
+++
@@ -1,5 +0,0 @@
-master_export <- returns_daily %>%
-    left_join(risk_metrics, by = "symbol") %>%
-    left_join(beta_data, by = "symbol")
-
-write.csv(master_export, "data/processed/tableau_master_dataset.csv", row.names = FALSE)
